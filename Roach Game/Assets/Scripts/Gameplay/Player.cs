39a1517f2645f1542109f4a8a013bce6d3a39859
Linden Reid <22460579+travislindenreid@users.noreply.github.com>
2026-09-08 14:53:29 -0700

    mold psychosis effect as percent of player health

--- Roach Game/Assets/Scripts/Gameplay/Player.cs
+++ Roach Game/Assets/Scripts/Gameplay/Player.cs
@@ -92,7 +92,7 @@ public class Player : MonoBehaviour
     public static Player _Instance { get; private set; }
 
     public Vector3 _Position => transform.position;
-    public float _HealthPercent => (float)_health / _maxHealth;
+    public float _HealthPercent => _health / _maxHealth;
     public bool _AtMaxHealth => _health == _maxHealth;
     public Transform _CameraTransform => _cameraTransform;
     public Vector3 _CameraPosition => _cameraTransform.position;
