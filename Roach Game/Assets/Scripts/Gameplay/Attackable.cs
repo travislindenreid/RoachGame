b7c59e317ea649911e4ac59c7a3651535139fdc7
Linden Reid <22460579+travislindenreid@users.noreply.github.com>
2026-09-09 18:38:23 -0700

    created parent class attackable for roaches

---
+++ Roach Game/Assets/Scripts/Gameplay/Attackable.cs
@@ -0,0 +1,40 @@
+/*
+ * Filename: /Users/lindenreid/Documents/GitHub/RoachGame/Roach Game/Assets/Scripts/Gameplay/Attackable.cs
+ * Path: /Users/lindenreid/Documents/GitHub/RoachGame/Roach Game/Assets/Scripts/Gameplay
+ * Created Date: Wednesday, September 9th 2026, 3:38:37 pm
+ * Author: Travis Reid
+ * 
+ * Copyright (c) 2026 Studio Tilia
+ */
+
+using UnityEngine;
+
+public class Attackable : MonoBehaviour
+{
+    // ------------------------------------------------------------------------
+    // Variables
+    // ------------------------------------------------------------------------
+    [Header("Attackable")]
+    [SerializeField] protected float _maxHealth = 1;
+
+    protected float _health;
+
+    // ------------------------------------------------------------------------
+    // Properties
+    // ------------------------------------------------------------------------
+    public bool _IsDead => _health <= 0;
+
+    // ------------------------------------------------------------------------
+    // Methods
+    // ------------------------------------------------------------------------
+    protected virtual void Start()
+    {
+        _health = _maxHealth;
+    } 
+
+    // ------------------------------------------------------------------------
+    public virtual void Hit ()
+    {
+        Debug.LogError("Attackable.Hit() not implemented.");
+    }
+}
